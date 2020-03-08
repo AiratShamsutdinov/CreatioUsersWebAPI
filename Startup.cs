ba2fa328119400b665c314a596d788e040fcadcf
AiratShamsutdinov <61822015+AiratShamsutdinov@users.noreply.github.com>
2020-03-08 22:17:21 +0300

    Отключил использование https

--- Startup.cs
+++ Startup.cs
@@ -92,7 +92,7 @@ namespace CreatioUsersWebApi
 				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
 			});
 
-			app.UseHttpsRedirection();
+			//app.UseHttpsRedirection();
 
 			app.UseRouting();
 
