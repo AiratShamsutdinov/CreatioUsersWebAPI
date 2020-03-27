77dd5cff6f1260173c3fc5cede807f7c247648bd
AiratShamsutdinov <61822015+AiratShamsutdinov@users.noreply.github.com>
2020-03-27 21:17:05 +0300

    Исправил разрешение на запросы от приложений

--- Startup.cs
+++ Startup.cs
@@ -52,9 +52,9 @@ namespace CreatioUsersWebApi
 				options.AddPolicy(CorsPolicyName,
 					builder =>
 					{
-						builder.WithOrigins("http://172.30.119.243:8080")
-							.AllowAnyHeader()
-							.AllowAnyMethod();
+						builder.AllowAnyOrigin()
+						       .AllowAnyHeader()
+						       .AllowAnyMethod();
 					});
 			});
 
